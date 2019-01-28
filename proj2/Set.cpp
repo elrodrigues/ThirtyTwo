7e9703d62ca08e077b615443561c108bb1e18bc1
elrodrigues <elvis@elrodrigues.com>
2019-01-27 21:51:23 -0800

    Implementations 1

--- proj2/Set.cpp
+++ proj2/Set.cpp
@@ -30,3 +30,79 @@ int Set::size() const
 {
   return m_size;
 }
+
+bool Set::contains(const ItemType& value) const
+{
+  Node* p;
+  int c = 0;
+  for(p = m_head; c < m_size && p->m_data != value; p = p->m_next)
+  {
+    c++;
+  }
+  return (c == m_size);
+}
+
+bool Set::get(int pos, ItemType& value) const
+{
+  Node* p = m_head;
+  if(pos < 0 || pos >= m_size)
+    return false;
+  for(int i = 0; i < pos; i++)
+    p = p->m_next;
+  value = p->m_date;
+  return true;
+}
+
+bool Set::insert(const ItemType& value)
+{
+  if(m_head == nullptr) // "INITIALIZES A SET"
+  {
+    m_head = new Struct();
+    m_size = 1;
+    m_head->m_next = m_head;
+    m_head->m_prev = m_head;
+    m_head->m_data = value;
+    return true;
+  }
+  if(this->contains(value)) // CHECKS IF ALREADY IN SET
+    return false;
+
+  Node* p;
+  Node* prv;
+  Node* newGuy = new Struct();
+  newGuy->m_data = value;
+
+  int c = 0;
+
+  for(p = m_head; c < m_size; p = p->next)
+  {
+    c++;
+    if(p->m_data > value) // INSERTION
+    {
+
+      prv = p->m_prev;
+      newGuy->m_next = p;
+      newGuy->m_prev = prv;
+
+      prv->m_next = newGuy;
+
+      p->m_prev = newGuy;
+      if(p == m_head)
+      {
+        m_head = newGuy;
+      }
+      ++m_size;
+      return true;
+    }
+  }
+  // INSERTION AT END
+  p = p->m_prev; // Set to last node
+  newGuy->m_next = m_head;
+  newGuy->m_prev = p;
+
+  p->m_next = newGuy;
+
+  m_head->m_prev = newGuy;
+  return true;
+
+}
