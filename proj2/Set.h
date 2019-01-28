9e4630d31405339b5182d19057d8da994d3807f8
elrodrigues <elvis@elrodrigues.com>
2019-01-27 20:41:56 -0800

    Setup Set.h

--- proj2/Set.h
+++ proj2/Set.h
@@ -1,4 +1,33 @@
 #ifndef SET_INCLUDED
 #define SET_INCLUDED
+#include <string>
+
+using ItemType = unsigned long;
+int MAX_DEFAULT_ITEMS = 24
+
+class Set
+{
+public:
+  Set();
+  ~Set(); // Destructor
+  Set(const Set& other); // Copy Constructor
+  Set& operator=(const Set& rhs); // Assignment Overloader
+
+  bool empty() const;
+  int size() const;
+  bool insert(const ItemType& value);
+  bool erase(const ItemType& value);
+  bool contains(const ItemType& value) const;
+  bool get(int pos, ItemType& value) const;
+  void swap (Set& other);
+
+private:
+  struct Node // Class for node in linked list.
+  {
+    ItemType m_data;
+    Node* next;
+  }
+  Node* head; // Pointer to starting node.
+}
 
 #endif
