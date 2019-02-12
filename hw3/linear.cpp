686bee79d71501cdc8bf91c7322ceab77324a268
elrodrigues <elvis@elrodrigues.com>
2019-02-12 12:36:44 -0800

    fluff removed

--- hw3/linear.cpp
+++ hw3/linear.cpp
@@ -1,6 +1,3 @@
-
-  // Return true if the somePredicate function returns true for at
-  // least one of the array elements, false otherwise.
 bool anyTrue(const double a[], int n)
 {
     if(n <= 0)
@@ -10,8 +7,6 @@ bool anyTrue(const double a[], int n)
     return anyTrue(a + 1, n - 1);
 }
 
-  // Return the number of elements in the array for which the
-  // somePredicate function returns true.
 int countTrue(const double a[], int n)
 {
     if(n <= 0) // STOPGAP
@@ -22,9 +17,6 @@ int countTrue(const double a[], int n)
 
 }
 
-  // Return the subscript of the first element in the array for which
-  // the somePredicate function returns true.  If there is no such
-  // element, return -1.
 int firstTrue(const double a[], int n)
 {
   if(n <= 0) // Not Found
@@ -39,10 +31,6 @@ int firstTrue(const double a[], int n)
 
 }
 
-  // Return the subscript of the smallest element in the array (i.e.,
-  // return the smallest subscript m such that a[m] <= a[k] for all
-  // k from 0 to n-1).  If the function is told to examine no
-  // elements, return -1.
 int positionOfMin(const double a[], int n)
 {
     if(n <= 0)
@@ -56,21 +44,6 @@ int positionOfMin(const double a[], int n)
       return n - 1;
 }
 
-  // If all n2 elements of a2 appear in the n1 element array a1, in
-  // the same order (though not necessarily consecutively), then
-  // return true; otherwise (i.e., if the array a1 does not include
-  // a2 as a not-necessarily-contiguous subsequence), return false.
-  // (Of course, if a2 is empty (i.e., n2 is 0), return true.)
-  // For example, if a1 is the 7 element array
-  //    10 50 40 20 50 40 30
-  // then the function should return true if a2 is
-  //    50 20 30
-  // or
-  //    50 40 40
-  // and it should return false if a2 is
-  //    50 30 20
-  // or
-  //    10 20 20
 bool includes(const double a1[], int n1, const double a2[], int n2)
 {
     if(n2 <= 0)
