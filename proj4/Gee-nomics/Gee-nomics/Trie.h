36503371092444a267d5e6c3fea2a7f257e2e51e
elrodrigues <elvis@elrodrigues.com>
2019-03-09 13:56:11 -0800

    trie insert

--- proj4/Gee-nomics/Gee-nomics/Trie.h
+++ proj4/Gee-nomics/Gee-nomics/Trie.h
@@ -4,8 +4,6 @@
 #include <string>
 #include <vector>
 
-int NUM_BASE = 4;
-
 template<typename ValueType>
 class Trie
 {
@@ -19,33 +17,48 @@ public:
       // C++11 syntax for preventing copying and assignment
     Trie(const Trie&) = delete;
     Trie& operator=(const Trie&) = delete;
+
+    //// DELETE!
+    bool test(const std::string& key) const;
 private:
     struct Node
     {
-      std::string m_label;
+      Node();
+      char m_label;
       std::vector<ValueType> m_val;
-      Node* m_child[NUM_BASE];
+      std::vector<Node*> m_child;
+      bool hasNoChildren;
     };
     Node* m_root;
-    bool hasNoChildren;
-    void deleteAllNodes(Node*& ptr);
-    bool isChildrenEmpty(Node*& ptr) const;
-    bool findByRecursion(const std::string& key, bool exactMatchOnly, int& pos, Node* ptr, Node*& res) const;
-};
-// PRIVATE: ARRAY VER
-template<typename ValueType>
-bool Trie<ValueType>::isChildrenEmpty(Node*& ptr)
-{
-  return haveNoChildren;
-}
 
+    void deleteAllNodes(Node*& ptr);
+    int findByRecursion(const std::string& key, bool exactMatchOnly, int pos, Node* ptr, Node*& res) const;
+    Node* insertNode(Node* ptr, char c);
+
+
+};
+// PRIVATE
+template<typename ValueType>
+Trie<ValueType>::Node::Node()
+: hasNoChildren(true)
+{}
+
+template<typename ValueType> // DELETE
+bool Trie<ValueType>::test(const std::string& key) const
+{
+  Node* p = nullptr;
+  bool f = findByRecursion(key, true, 0, m_root, p) == key.size();
+  for(size_t i = 0; i < p->m_val.size(); i++)
+    std::cerr << p->m_val[i] << std::endl;
+  return f;
+}
 
 template<typename ValueType>
 void Trie<ValueType>::deleteAllNodes(Node*& ptr)
 {
   if(ptr == nullptr || ptr->hasNoChildren)
     return;
-  for(int t = 0; t < NUM_BASE; t++)
+  for(std::size_t t = 0; t < ptr->m_child.size(); t++)
   {
     deleteAllNodes(ptr->m_child[t]);
     delete ptr->m_child[t];
@@ -53,26 +66,49 @@ void Trie<ValueType>::deleteAllNodes(Node*& ptr)
 }
 
 template<typename ValueType>
-bool Trie<ValueType>::findByRecursion(const std::string& key,
-  bool exactMatchOnly, int& pos, Node* ptr, Node*& res) const
-{ // Make position, ptr ref. Set return to bool instead.
-  if(pos == key.size())
+int Trie<ValueType>::findByRecursion(const std::string& key,
+  bool exactMatchOnly, int pos, Node* ptr, Node*& res) const
+{
+  if(pos == key.size() || ptr == nullptr || ptr->hasNoChildren)
   {
     res = ptr;
-    return true;
+    return pos;
   }
-  if(ptr == nullptr || ptr->hasNoChildren)
-    return false;
-  int p = pos + 1;
-  for(int i = 0; i < NUM_BASE; i++)
+  int low = 0; int high = ptr->m_child.size() - 1;
+  int mid = (low + high) / 2;
+  while(low <= high)
   {
-      if(findByRecursion(key, exactMatchOnly, p, ptr->m_child[1], res))
-      {
-
-      }
+    char c = ptr->m_child[mid]->m_label;
+    if(c == key[pos])
+      return findByRecursion(key, exactMatchOnly, pos + 1, ptr->m_child[mid], res);
+    (c < key[pos]) ? (low = mid + 1) : (high = mid - 1);
+    mid = (low + high) / 2;
   }
+  res = ptr;
+  return pos;
 }
 
+template<typename ValueType>
+typename Trie<ValueType>::Node* Trie<ValueType>::insertNode(Node* ptr, char c)
+{
+  Node* n = new Node();
+  n->m_label = c;
+  if(ptr->m_child.size() == 0)
+  {
+    ptr->m_child.push_back(n);
+    ptr->hasNoChildren = false;
+  }
+  else
+  {
+    for(typename std::vector<Node*>::iterator p = ptr->m_child.begin(); p != ptr->m_child.end(); p++)
+      if((*p)->m_label > c)
+      {
+        ptr->m_child.insert(p, n);
+        break;
+      }
+  }
+  return n;
+}
 // PUBLIC
 template<typename ValueType>
 Trie<ValueType>::Trie()
@@ -90,12 +126,20 @@ Trie<ValueType>::~Trie()
 template<typename ValueType>
 void Trie<ValueType>::insert(const std::string& key, const ValueType& value)
 {
-  // int index = 0;
-  // Node* ptr = m_root
-  // for(int i = 0; i < key.size(); i++)
-  // {
-  //   if()
-  // }
+  if(m_root == nullptr)
+  {
+    m_root = new Node();
+    m_root->m_child.push_back(new Node());
+    m_root->m_child[0]->m_label = key[0];
+    m_root->hasNoChildren = false;
+  }
+  Node* r = nullptr;
+  int p = findByRecursion(key, true, 0, m_root, r);
+  for(; p < key.size(); p++)
+  {
+    r = insertNode(r, key[p]);
+  }
+  r->m_val.push_back(value);
 }
 
 template<typename ValueType>
--- proj4/Gee-nomics/Gee-nomics/main.cpp
+++ proj4/Gee-nomics/Gee-nomics/main.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main()
 {
-  Trie<int>* test = new Trie<int>;
-  test->reset();
+  Trie<int>* t = new Trie<int>;
+  t->insert("ATG", 4);
+  cerr << t->test("ATG") << endl;
+  t->insert("ACG", 9);
+  cerr << t->test("ACG") << endl;
+  t->insert("ACG", 69);
+  cerr << t->test("ACG") << endl;
+  t->insert("ATG", 420);
+  cerr << t->test("ATG") << endl;
+  cerr << t->test("AT") << endl;
+
+  t->insert("ACGTG", 55);
+  cerr << t->test("ACGTG") << endl;
   cerr << "Test Passed" << endl;
-  delete test;
+  delete t;
 }
